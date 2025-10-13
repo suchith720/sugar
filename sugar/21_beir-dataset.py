8a8a4316a94e95ae27022c0323a5febfd050a4b7
Suchith Prabhu <suchith720@gmail.com>
2025-10-13 16:41:24 +0530

    beir download and data creation

--- sugar/21_beir-dataset.py
+++ sugar/21_beir-dataset.py
@@ -14,7 +14,7 @@ from dataclasses import dataclass
 from huggingface_hub import snapshot_download
 from typing import Optional, Dict, Tuple, List
 
-from sugar.core import *
+from .core import *
 
 # %% ../nbs/21_beir-dataset.ipynb 4
 def unzip_file(input_file, output_file):
