508d96ec6b98d948b197a844aacad22e229e5734
Suchith Prabhu <suchith720@gmail.com>
2025-11-08 13:31:28 +0000

    create dataset

--- scripts/02-create_dataset.sh
+++ scripts/02-create_dataset.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-datasets="arguana climate-fever dbpedia-entity fever fiqa hotpotqa nfcorpus nq quora scidocs scifact webis-touche2020 trec-covid \
+datasets="msmarco arguana climate-fever dbpedia-entity fever fiqa hotpotqa nfcorpus nq quora scidocs scifact webis-touche2020 trec-covid \
 	cqadupstack/android cqadupstack/english cqadupstack/gaming cqadupstack/gis cqadupstack/mathematica cqadupstack/physics \
 	cqadupstack/programmers cqadupstack/stats cqadupstack/tex cqadupstack/unix cqadupstack/webmasters cqadupstack/wordpress"
 
 for dset in $datasets
 do
-	python sugar/21_beir-dataset.py --data_dir /data/datasets/beir/$dset/ --save_dir /data/datasets/beir/$dset/XC/
+	data_dir="/data/datasets/beir"
+	save_dir="/data/datasets/beir"
+
+	python sugar/21_beir-dataset.py --data_dir $data_dir/$dset/ --save_dir $save_dir/$dset/XC/ 
 
 	echo $dset
 	echo ---------------
 
-	data_dir="/data/datasets/beir"
-	files="$data_dir/$dset/XC/trn_X_Y.npz $data_dir/$dset/XC/tst_X_Y.npz $data_dir/$dset/XC/raw_data/train.raw.csv \
-		$data_dir/$dset/XC/raw_data/test.raw.csv $data_dir/$dset/XC/raw_data/label.raw.csv"
+	files="$save_dir/$dset/XC/trn_X_Y.npz $save_dir/$dset/XC/tst_X_Y.npz $save_dir/$dset/XC/raw_data/train.raw.csv \
+		$save_dir/$dset/XC/raw_data/test.raw.csv $save_dir/$dset/XC/raw_data/label.raw.csv"
 	for file in $files
 	do
 		if [ ! -f $file ]
